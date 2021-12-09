673f0a5e110b619cfc1c74a7914276cebfaec147
Logan <84389612+llbokilo@users.noreply.github.com>
2021-12-09 15:23:46 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';  
+import {Platform, StyleSheet, Text, View} from 'react-native';  
+  
+type Props = {};  
+export default class App extends Component<Props> {  
+  render() {  
+    return (  
+      <View>  
+        <Text>Logan vous dit:</Text>  
+        <Text>Hello World!!!</Text>  
+      </View>  
+    );  
+  }  
+}
