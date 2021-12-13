4088e8a2944de97aaff1353374bd5ffb33f89500
Logan <84389612+llbokilo@users.noreply.github.com>
2021-12-13 11:31:35 +0100

    MAJ

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
-import About from "./components/About";
-import Home from "./components/Home";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-const Tab = createBottomTabNavigator();
 
 export default class App extends Component {
   render() {
     return (
       <View>
         <Text>Waaaaaaaaaah</Text>
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Maison" component={Home} />
-            <Tab.Screen name="A Propos" component={About} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <Text>Bonjour, bonjour!</Text>
+        <Text>Hello Android, ceci est mon premier code React Native ^^</Text>
       </View>
     );
   }
